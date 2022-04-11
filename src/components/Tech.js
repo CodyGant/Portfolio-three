8272448019cf0e6545885ad1316dfa1c1e1630fa
Cody Gant <46806763+CodyGant@users.noreply.github.com>
2022-04-11 13:38:21 -0400

    adding nextjs logo

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -3,6 +3,7 @@ import JavaScript from "../components/BodyAnimation";
 import Html from "../components/HtmlAnimation";
 import Css from "../components/CssAnimation";
 import Reactlogo from "../components/Reactlogoanimation";
+import Nextjs from '../components/nextjslogoanimation'
 import { Card, CardGroup } from "react-bootstrap";
 function Tech() {
   return (
@@ -30,6 +31,11 @@ function Tech() {
           <Card>
             <Card.Body>
               <Reactlogo />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Nextjs />
             </Card.Body>
           </Card>
         </CardGroup>
