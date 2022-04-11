f8059ce48a45cd5d181037bcbdfc7b4b548fe4e9
Cody Gant <46806763+CodyGant@users.noreply.github.com>
2022-04-11 12:51:38 -0400

    added the react logo

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -2,6 +2,7 @@ import React from "react";
 import JavaScript from "../components/BodyAnimation";
 import Html from "../components/HtmlAnimation"
 import Css from "../components/CssAnimation"
+import Reactlogo from '../components/Reactlogoanimation'
 import {Card, CardGroup} from "react-bootstrap";
 function Tech() {
   return (
@@ -22,6 +23,11 @@ function Tech() {
         <Card>
           <Card.Body>
             <Css />
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Reactlogo />
           </Card.Body>
         </Card>
         
