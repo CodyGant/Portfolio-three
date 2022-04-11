943cfce7399573268457d4f11952dfe4067be900
Cody Gant <46806763+CodyGant@users.noreply.github.com>
2022-04-11 12:55:13 -0400

    updated react logo location

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,36 +1,37 @@
 import React from "react";
 import JavaScript from "../components/BodyAnimation";
-import Html from "../components/HtmlAnimation"
-import Css from "../components/CssAnimation"
-import Reactlogo from '../components/Reactlogoanimation'
-import {Card, CardGroup} from "react-bootstrap";
+import Html from "../components/HtmlAnimation";
+import Css from "../components/CssAnimation";
+import Reactlogo from "../components/Reactlogoanimation";
+import { Card, CardGroup } from "react-bootstrap";
 function Tech() {
   return (
-    <div className="Technologies"  id = 'Technologies'>
+    <div className="Technologies" id="Technologies">
       <div className="fullbody">
         <h1>Technologies</h1>
         <CardGroup>
-        <Card className = 'card'>
-          <Card.Body >
-            <JavaScript />
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Html/>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Css />
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Reactlogo />
-          </Card.Body>
-        </Card>
-        
+          <Card className="card">
+            <Card.Body>
+              <JavaScript />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Html />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Css />
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <CardGroup>
+          <Card>
+            <Card.Body>
+              <Reactlogo />
+            </Card.Body>
+          </Card>
         </CardGroup>
       </div>
     </div>
