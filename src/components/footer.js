50c47745bf7737bbf5f8c8a85016ad446b4af634
cody gant <codygantol@gmail.com>
2022-07-29 16:15:18 -0400

    updated text

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Skeleton from '../components/Skeleton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 function footer() {
   return (
     <div className="footerContainer">
@@ -7,15 +9,14 @@ function footer() {
       <div className = 'skeletonBox'>
       <p>Goodbye, I hope to hear from you.</p>
         <Skeleton/>
-       
         </div>
       
-        <div class="footer-left">
+        <div className="footer-left">
           
           <h3>
             <span></span>
           </h3>
-          <p class="footer-links">
+          <p className="footer-links">
             <a href="\">HOME</a>
             <a href="\">RESUME</a>           
             <a href="\">CONTACT</a>
@@ -24,45 +25,45 @@ function footer() {
           
         </div>
 
-        <div class="footer-center">
+        <div className="footer-center">
           <div>
-            <i class="fa fa-map-marker"></i>
+            <i className="fa fa-map-marker"></i>
             <p>
               <span>Based in Central Florida,</span> USA.
             </p>
           </div>
 
           <div>
-            <i class="fa fa-phone"></i>
-            <p>(386) 882-4486</p>
+            <i className="fa fa-phone"></i>
+            <p><a href="tel:3868824486">(386) 882-4486</a></p>
           </div>
 
           <div>
-            <i class="fa fa-envelope"></i>
+            <i className="fa fa-envelope"></i>
             <p>
-              <a href="mailto:support@company.com">codygantol@gmail.com</a>
+              <a href="mailto:codygantol@gmail.com">codygantol@gmail.com</a>
             </p>
           </div>
         </div>
 
-        <div class="footer-right">
-          <p class="footer-company-about">
+        <div className="footer-right">
+          <p className="footer-company-about">
           
             
           </p>
 
-          <div class="footer-icons">
+          <div className="footer-icons">
             <a href="\">
-              <i class="bi bi-facebook"></i>
+              <i className="bi bi-facebook"></i>
             </a>
             <a href="\">
-              <i class="bi bi-twitter"></i>
+            <FontAwesomeIcon icon={faCoffee} />
             </a>
             <a href="\">
-              <i class="bi bi-linkedin"></i>
+            <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
             </a>
             <a href="\">
-              <i class="bi bi-instagram"></i>
+              <i className="bi bi-instagram"></i>
             </a>
           </div>
         </div>
